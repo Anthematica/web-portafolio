e988f6c15c485313d421dc084e27de7609db78d3
Anthematica <fernando21112000@gmail.com>
2022-06-09 02:31:08 -0600

    Fixing resume dowload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
+import MyResume from '../assets/CVAnthony.pdf';
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
@@ -122,7 +123,7 @@ function Navbar() {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/src/assets/CVAnthony.pdf"
+              href={MyResume}
               download
             >
               Resume <BsFillPersonLinesFill size={30} />
